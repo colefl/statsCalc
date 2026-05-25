21cd665533ea67acd510baf296efcabc4cbabb3e
Cole F <153025792+colefl@users.noreply.github.com>
2026-05-25 17:15:56 -0500

    I finished the input and now I'm going to work on a fun little personal project :)

---
+++ ComAndPerm.cpp
@@ -0,0 +1,3 @@
+float calcPermutations(float val1, float val2){
+    
+}
--- io.cpp
+++ io.cpp
@@ -168,70 +168,99 @@ int pc_choice(){
 
 int perm_screen(){
     mvaddstr(DEFAULT_Y, 0, "Please input the values(select using arrow keys):");
-    mvaddstr(DEFAULT_Y + 2, 0, "P(___, ___)");
+    mvaddstr(DEFAULT_Y + 2, 0, "input 1: ___");
+	mvaddstr(DEFAULT_Y + 4, 0, "input 2: ___");
+	mvaddstr(DEFAULT_Y + 6, 0, "Calculate:");
     float value1, value2;
     int key, selected;
     refresh();
-    while((key = getch()) != 'q'){
-        switch(key){
-            case KEY_LEFT:
-            selected = 1;
-            attron(A_STANDOUT);
-            mvaddstr(DEFAULT_Y + 2, 2, "___");
-            refresh();
-            value1 = init_selection();
-            attroff(A_STANDOUT);
-            mvaddstr(DEFAULT_Y + 2, 2, "___");
-            move(DEFAULT_Y + 2, 2);
-            printw("%3.*f", value1);
-            break;
-            //mvaddstr(DEFAULT_Y + 2, 3, value1);
-            
-            case KEY_RIGHT:
-            selected = 2;
-            attron(A_STANDOUT);
-            mvaddstr(DEFAULT_Y + 2, 7, "___");
-            refresh();
-            value2 = init_selection();
-            move(DEFAULT_Y + 2, 7);
-            attroff(A_STANDOUT);
-            printw("%3.*f", value2);
-            break;
-        }
-        refresh();
+	while((key = getch() != 'q')){
+		//Kinda just a scroll
+		while((key = getch()) != 'f'){
+			switch(key){
+				case KEY_LEFT:
+				selected -= 1;
+				//Okay so I still need to light up the correct one
+				//mvaddstr(DEFAULT_Y + 2, 3, value1);
+				
+				case KEY_RIGHT:
+				selected += 1;
+			}
+			if(selected == 0){
+				selected = 3;
+			} else if(selected == 1){
+				attron(A_STANDOUT);
+				mvaddstr(DEFAULT_Y + 2, 9, "___");
+				refresh();
+			} else if(selected == 2){
+				attron(A_STANDOUT);
+				mvaddstr(DEFAULT_Y + 4, 9, "___");
+				refresh();
+			} else if(selected == 3){
+				attron(A_STANDOUT);
+				mvaddstr(DEFAULT_Y + 6, 0, "Calculate");
+				refresh();
+			}
+		}
+        //refresh();
+		if(selected == 1){
+			value1 = init_selection();
+			//std::cout << value1 << std::endl;
+			attroff(A_STANDOUT);
+			mvaddstr(DEFAULT_Y + 2, 9, "___");
+			move(DEFAULT_Y + 2, 9);
+			printw("%f", value1);
+		} else if(selected == 2){
+			value2 = init_selection();
+			move(DEFAULT_Y + 4, 9);
+			attroff(A_STANDOUT);
+			printw("%f", value2);
+		} else if(selected == 3){
+			if(value1 == NULL || value2 == NULL){
+				mvaddstr(DEFAULT_Y + 8, 0, "Please input values for both numbers");
+			} else {
+				
+			}
+		}
+
+		refresh();
     }
     return selected;
 }
 
 float init_selection(){
     //char[100] placeholder;
-    char* returnValStr = new char [256];
+    //char* returnValStr = new char [256];
     float returnVal;
-    int key;
-    while((key = getch()) != 'q'){
+	std::cin >> returnVal;
+	//std::cout << returnVal << std::endl;
+    //int key;
+    //while((key = getch()) != 'q'){
         //const char* str_msg = String("%f3", returnVal);
         //std::cin >> returnValStr; //I don't think I understand how this works
-        switch(key){
-            case KEY_DL:
-                if(strlen(returnValStr) < 0){
-                    std::cout << "I make it to where the string doesn't" << std::endl;
-                    break;
-                } else {
-                    std::cout << "I make it to where the string has capacity" << std::endl;
-                    strncpy(returnValStr, returnValStr, sizeof (char) * (strlen(returnValStr) - 1)); //Deleting the last string
-                    break;
-                }
-            default:
-                std::cout << "I make it to default "  << returnValStr << " hello" << std::endl;
-                returnValStr += key;
-        }
-        //returnValStr += key;
-        returnVal = strtof(returnValStr, NULL);
-        std::cout << returnVal << std::endl;
-        //move(DEFAULT_Y + 2, 2);
-        //printw("%3.0f", returnVal);
-        refresh();
+        // switch(key){
+        //     case KEY_DL:
+        //         if(strlen(returnValStr) < 0){
+        //             std::cout << "I make it to where the string doesn't" << std::endl;
+        //             break;
+        //         } else {
+        //             std::cout << "I make it to where the string has capacity" << std::endl;
+        //             strncpy(returnValStr, returnValStr, sizeof (char) * (strlen(returnValStr) - 1)); //Deleting the last string
+        //             break;
+        //         }
+        //     default:
+        //         std::cout << "I make it to default "  << returnValStr << " hello" << std::endl;
+        //         returnValStr += key;
+        // }
+        // //returnValStr += key;
+        // returnVal = strtof(returnValStr, NULL);
+        // std::cout << returnVal << std::endl;
+        // //move(DEFAULT_Y + 2, 2);
+        // //printw("%3.0f", returnVal);
+        // refresh();
+
         
-    }
+    //}
     return returnVal;
 }
+
